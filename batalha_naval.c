978d289e43e40f6da03a92448cdb6b2e4b43f637
Calves <calves.dev@gmail.com>
2025-03-09 17:59:31 -0300

    Desafio nível aventureiro

--- batalha_naval.c
+++ batalha_naval.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+#define LINHAS 10
+#define COLUNAS 10
+
 int main()
 {
-    int tabuleiro[10][10] = {
+    int tabuleiro[LINHAS][COLUNAS] = {
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -13,21 +16,29 @@ int main()
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
-        navio1[3] = {3, 3, 3}, navio2[3] = {3, 3, 3}; // Declaração da matriz 10x10 do tabuleiro e arrays de tamanho 3 para os navios
+        navio1[3] = {3, 3, 3}; // Declaração da matriz 10x10 do tabuleiro e arrays de tamanho 3 para os navios
 
     tabuleiro[4][6] = navio1[0]; // Atribuição do navio 1 na horizontal
     tabuleiro[4][7] = navio1[1];
     tabuleiro[4][8] = navio1[2];
 
-    tabuleiro[7][8] = navio2[0]; // Atribuição do navio 2 na vertical
-    tabuleiro[8][8] = navio2[1];
-    tabuleiro[9][8] = navio2[2];
+    tabuleiro[7][8] = navio1[0]; // Atribuição do navio 2 na vertical
+    tabuleiro[8][8] = navio1[1];
+    tabuleiro[9][8] = navio1[2];
 
-    for (int i = 0; i < 10; i++)
+    tabuleiro[3][9] = navio1[0]; // Atribuição do navio 3 na diagonal
+    tabuleiro[2][8] = navio1[1];
+    tabuleiro[1][7] = navio1[2];
+
+    tabuleiro[6][4] = navio1[0]; // Atribuição do navio 4 na diagonal
+    tabuleiro[7][5] = navio1[1];
+    tabuleiro[8][6] = navio1[2];
+
+    for (int i = 0; i < LINHAS; i++)
     {
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < COLUNAS; j++)
         {
-            printf("%d", tabuleiro[i][j]);
+            printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
