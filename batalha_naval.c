d864a1cebd8c4a7205a71039cc3daf2e431a06f0
Calves <calves.dev@gmail.com>
2025-03-09 21:43:25 -0300

    Desafio nível mestre

--- batalha_naval.c
+++ batalha_naval.c
@@ -17,6 +17,7 @@ int main()
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
         navio1[3] = {3, 3, 3}; // Declaração da matriz 10x10 do tabuleiro e arrays de tamanho 3 para os navios
+    int cone[3][5], cruz[3][5], octaedro[3][5];
 
     tabuleiro[4][6] = navio1[0]; // Atribuição do navio 1 na horizontal
     tabuleiro[4][7] = navio1[1];
@@ -34,6 +35,40 @@ int main()
     tabuleiro[7][5] = navio1[1];
     tabuleiro[8][6] = navio1[2];
 
+    for (int i; i < 3; i++) // Laço para atribuição de valores das habilidades
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cone[i][j] = 0; // Inicializando as matrizes das habilidades
+            cruz[i][j] = 0;
+            octaedro[i][j] = 0;
+            if (((i == 0 && j == 2)) || (i == 1 && j != 0 && j != 4) || (i == 2)) // Condição para Cone receber seus valores 5
+            {
+                cone[i][j] = 5;
+            }
+            if ((i == 0 && j == 2) || (i == 1) || (i == 2 && j == 2)) // Condição para Cruz receber seus valores 5
+            {
+                cruz[i][j] = 5;
+            }
+            if ((i == 0 && j == 2) || (i == 1) && (j == 1 || j == 2 || j == 3) || (i == 2 && j == 2)) // Condição para Octaedro receber seus valores 5
+            {
+                octaedro[i][j] = 5;
+            }
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            {
+                tabuleiro[i + 4][j + 4] = cone[i][j];     // Inicia o cone nas coordenadas 4, 4
+                tabuleiro[i][j + 2] = cruz[i][j];         // Inicia a cruz nas coordenadas 0, 2
+                tabuleiro[i + 3][j + 1] = octaedro[i][j]; // Inicia o octaedro nas coordenadas 3, 2
+            }
+        }
+    }
+
     for (int i = 0; i < LINHAS; i++)
     {
         for (int j = 0; j < COLUNAS; j++)
@@ -42,6 +77,5 @@ int main()
         }
         printf("\n");
     }
-
     return 0;
 }
